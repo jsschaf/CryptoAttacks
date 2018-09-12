6143cb04393bdb8d05f39e70d052a6fa7f5480ea
jsschaf <jsschaf@umich.edu>
2018-09-12 12:47:32 -0400

    isolated token

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -2,6 +2,19 @@ import httplib, urlparse, sys
 url = sys.argv[1]
 
 # Your code to modify url goes here
+tokenStart = url.index("=") + 1
+tokenEnd = url.index("&")
+token = url[tokenStart:tokenEnd]
+
+
+#new command
+newCommand = "&command3=UnlockAllSafes"
+
+#create hash for new command ??
+h = md5()
+h.update(newCommand)
+suffix = h.hexdigest()
+
 
 parsedUrl = urlparse.urlparse(url)
 conn = httplib.HTTPSConnection(parsedUrl.hostname,parsedUrl.port)
