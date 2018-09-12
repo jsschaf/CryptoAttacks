9d1296dcfd8ad778c61f510c1e50731d94406977
jsschaf <jsschaf@umich.edu>
2018-09-12 13:33:48 -0400

    New Updates

--- len_ext_attack.py
+++ len_ext_attack.py
@@ -1,19 +1,27 @@
 import httplib, urlparse, sys
+from pymd5 import md5, padding
+
 url = sys.argv[1]
+
 
 # Your code to modify url goes here
 tokenStart = url.index("=") + 1
 tokenEnd = url.index("&")
 token = url[tokenStart:tokenEnd]
+print token
 
 
 #new command
 newCommand = "&command3=UnlockAllSafes"
 
+
 #create hash for new command ??
-h = md5()
+tokenLength = 8 + len(url[(tokenEnd + 1):]) #password + length of end part of URL
+numBits = 8*tokenLength
+h = md5(state = token.decode("hex"), count = numBits)
 h.update(newCommand)
 suffix = h.hexdigest()
+
 
 
 parsedUrl = urlparse.urlparse(url)
